535d382a00d9c7733a604c4a42ed41d19450f199
Falikha Sulfiyah <falikhasulfiyah@gmail.com>
2025-09-23 14:08:19 +0700

    Add files via upload

---
+++ PraktikumPBO_4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum4;
+
+/**
+ *
+ * @author hp
+ */
+public class PraktikumPBO_4 {
+
+    public static void main(String[] args) {
+        Kendaraan Lando = new Kendaraan("Mclaren MCL39", 257, "E10");
+        Lando.tampilkanInfoKendaraan();
+
+        mobil Oscar = new mobil("Mclaren MCL39", 218, "E10", 0);
+        Oscar.tampilkanInfomobil();
+    }
+}
